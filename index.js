0329468f6662ae2a8c5178e59183c055f8193909
Lui <lsanch78@asu.edu>
2025-08-01 15:09:02 -0700

    Errors involving async operation, need to implement promises in DNS lookup function

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ const mongoose = require('mongoose');
 let dbPass = process.env.MONGODB_PASS;
 const uri = "mongodb+srv://lsanch78:"+ dbPass + "@cluster0.ovob0h1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-// Url Schema Definition
+// Url Schema Definition, we will use MongoDB's unique ID for each document as the shortened URL
 const urlSchema = new mongoose.Schema({
     original_url: {type: String, required: true},
-    short_url: {type: Number, unique: true},
 })
 
 const Url = mongoose.model('Url', urlSchema);
@@ -62,44 +61,45 @@ app.get('/api/hello', function(req, res) {
 });
 
 
-app.post('/api/shorturl', function(req, res) {
-  // get url from body
- const originalUrl = req.body.url;
+app.post('/api/shorturl', async function (req, res) {
+    // get url from body
+    const originalUrl = req.body.url;
 
-  // create a hostname var to hold names after protocol ie. 'www.google.com'
- let hostname;
+    // create a hostname var to hold names after protocol ie. 'www.google.com'
+    let hostname;
 
- // try / catch to try and get the protocol from the originalUrl
-  try {
-    const urlObj = new URL(originalUrl);
-    if (urlObj.protocol !== "https:" && urlObj.protocol !== "http:") {
-      console.log("I got here");
-      return res.json({error: 'invalid url'});
-    }
-  // assign hostname the name after successfully parsing the protocol
-  hostname = urlObj.hostname;
+    // try / catch to try and get the protocol from the originalUrl
+    try {
+        const urlObj = new URL(originalUrl);
+        if (urlObj.protocol !== "https:" && urlObj.protocol !== "http:") {
+            console.log("I got here");
+            return res.json({error: 'invalid url'});
+        }
+        // assign hostname the name after successfully parsing the protocol
+        hostname = urlObj.hostname;
 
-  // if protocol cannot be parsed return json error
-  } catch (e) {
-    return res.json({error: 'url'});
-  }
-
-  // set servers to Google and Cloudfare so that it can automatically verify from their end (ISP's can intercept this and return false pos)
-  dns.setServers(['8.8.8.8', '1.1.1.1']); // Google + Cloudflare
-  dns.resolve4(hostname,(err, address) => {
-    if (err) {
-      console.log(err, address);
-      return res.json({error: 'invalid url'});
+        // if protocol cannot be parsed return json error
+    } catch (e) {
+        return res.json({error: 'url'});
     }
 
-    // shortened url logic goes here
-    const shortUrl = Math.floor(Math.random() * 1000);
-
-
-
-    return res.json({ original_url: originalUrl , short_url: shortUrl});
-  }
-);
+    // set servers to Google and Cloudfare so that it can automatically verify from their end (ISP's can intercept this and return false pos)
+    dns.setServers(['8.8.8.8', '1.1.1.1']); // Google + Cloudflare
+    dns.resolve4(hostname, async (err, address) => {
+            if (err) {
+                console.log(err, address);
+                return res.json({error: 'invalid url'});
+            }
+            // save to mongo DB and use unique ID
+            try {
+                const newUrl = new Url({original_url: originalUrl});
+                const saved = await newUrl.save();
+                res.json({original_url: saved.original_url, short_url: saved._id});
+            } catch (e) {
+                return res.json({error: 'mongo db server error'});
+            }
+        }
+    );
 });
 
 
