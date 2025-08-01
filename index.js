3ff9fe6379f1818d263053dac832c1ee28d0611d
Lui <lsanch78@asu.edu>
2025-08-01 14:25:25 -0700

    implemented URL verification logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
@@ -10,6 +12,10 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +24,48 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+app.post('/api/shorturl', function(req, res) {
+  // get url from body
+ const originalUrl = req.body.url;
+
+  // create a hostname var to hold names after protocol ie. 'www.google.com'
+ let hostname;
+
+ // try / catch to try and get the protocol from the originalUrl
+  try {
+    const urlObj = new URL(originalUrl);
+    if (urlObj.protocol !== "https:" && urlObj.protocol !== "http:") {
+      console.log("I got here");
+      return res.json({error: 'invalid url'});
+    }
+  // assign hostname the name after successfully parsing the protocol
+  hostname = urlObj.hostname;
+
+  // if protocol cannot be parsed return json error
+  } catch (e) {
+    return res.json({error: 'url'});
+  }
+
+  // set servers to Google and Cloudfare so that it can automatically verify from their end (ISP's can intercept this and return false pos)
+  dns.setServers(['8.8.8.8', '1.1.1.1']); // Google + Cloudflare
+  dns.resolve4(hostname,(err, address) => {
+    if (err) {
+      console.log(err, address);
+      return res.json({error: 'invalid url'});
+    }
+
+    // shortened url logic goes here
+    const shortUrl = Math.floor(Math.random() * 1000);
+
+
+
+    return res.json({ original_url: originalUrl , short_url: shortUrl});
+  }
+);
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
